24b6537570066dafbc797d33784bce5e2220ab04
andresOchoaHernandez <ada.wong.gv@gmail.com>
2021-02-11 23:23:12 +0100

    fixed script

--- fix_dataset.py
+++ fix_dataset.py
@@ -2,8 +2,10 @@ import glob
 import os
 import shutil
 
-books = [files for files in glob.glob("/home/andres/Gutenberg/txt/*.txt")]
-books = [elem.replace("/home/andres/Gutenberg/txt/", "") for elem in books]
+home_dir = os.environ["HOME"]
+
+books = [files for files in glob.glob(home_dir + "/Gutenberg/txt/*.txt")]
+books = [elem.replace(home_dir +"/Gutenberg/txt/", "") for elem in books]
 
 authors = [elem[:elem.find("_")] for elem in books]
 authors = list(set(authors))
@@ -11,13 +13,13 @@ authors.sort()
 
 books_per_author = {}
 for author in authors:
-	books_per_author[author] = [files for files in glob.glob("/home/andres/Gutenberg/txt/"+ author+"*.txt")]
+	books_per_author[author] = [files for files in glob.glob(home_dir +"/Gutenberg/txt/"+ author+"*.txt")]
 
-os.mkdir("/home/andres/Dataset/")
+os.mkdir(home_dir +"/Dataset/")
 for elem in authors:
-	os.mkdir("/home/andres/Dataset/"+elem)
+	os.mkdir(home_dir +"/Dataset/"+elem)
 
 for key in books_per_author.keys():
 	for path in books_per_author[key]:
-		shutil.copyfile(path,"/home/andres/Dataset/"+key+"/"+ path.replace("/home/andres/Gutenberg/txt/", ""))
+		shutil.copyfile(path,home_dir +"/Dataset/"+key+"/"+ path.replace(home_dir +"/Gutenberg/txt/", ""))
 		
